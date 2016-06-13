99554c3dbff2087bc11ef1a6d9def09c4ea9f89d
markszabo <torin42@gmail.com>
2016-06-13 14:56:00 +0200

    Reverting listDrones

--- code/listDrones.py
+++ code/listDrones.py
@@ -4,7 +4,6 @@ from evolo import scanForParrots, disconnectFromWifi
 
 disconnectFromWifi(interfaceForScan)
 parrots = scanForParrots(interfaceForScan, [], [])
-#parrots = scanForParrots("wlan0", [], [])
 
 for parrot in parrots:
-	logging.INFO("listDrones;" + parrot.ssid + ";" + parrot.address + ";" + str(parrot.signal))
+	print "listDrones;" + parrot.ssid + ";" + parrot.address + ";" + str(parrot.signal)
